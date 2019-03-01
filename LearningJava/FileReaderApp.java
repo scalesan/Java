31492ce121fe80113f3f2d451a6ff0b568249104
Scales Anthony (LJC0QBF) <LJC0QBF@UPS.COM>
2019-03-01 10:32:58 -0500

    More Java Work

--- LearningJava/FileReaderApp.java
+++ LearningJava/FileReaderApp.java
@@ -1,9 +1,44 @@
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 
 
 public class FileReaderApp{
     public static void main(String[] args){
-        File file = new File('test2.txt');
+        File file = new File("test2.txtx");
+        BufferedReader br = null;
+
+        try{
+            FileReader fr = new FileReader(file);
+            br = new BufferedReader(fr);
+
+            String line;
+            
+            while((line = br.readLine()) != null){
+
+            System.out.println(line);
+            }
+
+
+        } catch (FileNotFoundException e){
+            System.out.println("File Not Found: " + file.toString());
+        }
+         catch(IOException e){
+             System.out.println("Unable to read file: " + file.toString() );
+         }
+
+         finally{
+            try{
+                br.close();
+                } catch (IOException e){
+                    System.out.println("Unable to close file: " + file.toString());
+                } catch(NullPointerException ex){
+                   //File was probably never opened!
+                }
+
+         }
     }
 }
